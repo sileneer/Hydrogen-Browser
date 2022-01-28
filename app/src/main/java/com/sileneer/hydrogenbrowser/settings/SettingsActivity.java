35435567a2cb4b20cd1ace3dd0a14e0d55fea1de
Liu Zihao <yuan95338@gmail.com>
2022-01-29 00:52:07 +0800

    fix: keyboard will not hide when dialog dismiss

--- app/src/main/java/com/sileneer/hydrogenbrowser/settings/SettingsActivity.java
+++ app/src/main/java/com/sileneer/hydrogenbrowser/settings/SettingsActivity.java
@@ -1,16 +1,14 @@
 package com.sileneer.hydrogenbrowser.settings;
 
-import android.app.AlertDialog;
+import android.app.Activity;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.ViewGroup;
+import android.view.View;
 
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.sileneer.hydrogenbrowser.MainActivity;
 import com.sileneer.hydrogenbrowser.common.base.BaseActivity;
 import com.sileneer.hydrogenbrowser.R;
 import com.sileneer.hydrogenbrowser.common.utils.TitleLayout;
@@ -18,7 +16,7 @@ import com.sileneer.hydrogenbrowser.common.utils.TitleLayout;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SettingsActivity extends BaseActivity {
+public class SettingsActivity extends BaseActivity  {
 
     private List<Settings> settingsList = new ArrayList<>();
 
@@ -35,7 +33,7 @@ public class SettingsActivity extends BaseActivity {
         RecyclerView recyclerview = findViewById(R.id.recycler_view);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerview.setLayoutManager(layoutManager);
-        SettingsAdapter adapter = new SettingsAdapter(settingsList);
+        SettingsAdapter adapter = new SettingsAdapter(settingsList, this);
         recyclerview.setAdapter(adapter);
     }
 
@@ -43,54 +41,6 @@ public class SettingsActivity extends BaseActivity {
         for (String settings_item_name: settings_items) {
             settingsList.add(new Settings(settings_item_name));
         }
-    }
-
-    protected static void showSearchEngines(ViewGroup parent) {
-        AlertDialog.Builder ad = new AlertDialog.Builder(parent.getContext());
-        ad.setTitle("Please select your search engine:");
-
-        ad.setSingleChoiceItems(MainActivity.searchEngines, MainActivity.searchEnginesIndex, new DialogInterface.OnClickListener() {
-
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                MainActivity.editor_searchEngines.putInt("search engines", which);
-                MainActivity.editor_searchEngines.commit();
-            }
-        });
-        ad.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                MainActivity.searchEnginesIndex = MainActivity.sharedPref_searchEngines.getInt("search engines", 0);
-                MainActivity.changeAddressBarHint(MainActivity.searchEnginesIndex);
-            }
-        });
-
-        ad.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
-            }
-        });
-        ad.show();
-    }
-
-    protected static void showOpenSource(ViewGroup parent) {
-        AlertDialog.Builder ad = new AlertDialog.Builder(parent.getContext());
-        ad.setTitle("Open Source");
-        ad.setMessage("You will be redirected github.com. Are you sure to continue?");
-        ad.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int i) {
-                MainActivity.actionStart(parent.getContext());
-                MainActivity.loadOpenSource();
-            }
-        });
-        ad.setNegativeButton("No", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int i) {
-            }
-        });
-        ad.show();
     }
 
     public static void actionStart(Context context) {
